a5b0c7e7b8e6ad500662d50232a46ff9e3849f51
MD MOSTAKIN ISLAM <mostakin.marketer@gmail.com>
2023-08-29 12:39:45 +0600

    manu

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(function(){
 
     // nav manu toggle
     $(".mobile-menu-toggle").on("click", function () {
-        $(".popup-box-active").fadeToggle();
+      $(".popup-box-active").fadeToggle();
       });
     
       $(".popup-box-active .nav-manu ul li a").each(function () {
@@ -18,10 +18,39 @@ $(function(){
       $(".popup-box-active .nav-manu ul li .parrenttow").on("click", function () {
         $(".sub-icone2").toggleClass("arrowm-down");
         });
+        
       $(".popup-box-active .nav-manu ul li .parentthere").on("click", function () {
         $("#sub-icone3").toggleClass("arrowm-down");
         });
-      
+
+
+
+        // hero section
+        
+      $(".hero-btn").on("click", function () {
+        $(this).toggleClass("activebtn");
+        // $(".used, .new").css(
+        //   {
+        //     position: 'absolute',
+        //     top: '19px',
+        //     left: '26.6%'
+        //   }
+        // );
+
+        var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ filter: filterValue });
+        });
+        var $grid = $('.grid').isotope();
+
+        $('.serch-btn').each( function( i, buttonGroup ) {
+          var $buttonGroup = $( buttonGroup );
+          $buttonGroup.on( 'click', 'button', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $( this ).addClass('is-checked');
+          });
+        });
+        
+
 
 });
 
